506ae9594b1ee9aa1e61ddda328b70d193ffa3bc
Mohamed Mayaz <momayaz@e1r8p8.1337.ma>
2022-12-16 20:49:33 +0100

    1337 cursus

--- lvl_5/inception/inception/srcs/requirements/bonus/ftp/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-adduser -h /var/www -D ${FTP_USER}
-echo ${FTP_USER}:${FTP_PASSWORD} | chpasswd
-adduser ${FTP_USER} root
-exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
--- lvl_5/inception/inception/srcs/requirements/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p ~/data
-mkdir -p ~/data/mariadb
-mkdir -p ~/data/wordpress
-mkdir -p ~/data/portainer
